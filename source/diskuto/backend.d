0c4f5d85752fdf1d81990a80409a51497b3f2a44
Sönke Ludwig <sludwig@rejectedsoftware.com>
2017-03-16 18:54:45 +0100

    Add backwards-compatibility modules.

---
+++ source/diskuto/backend.d
@@ -0,0 +1,4 @@
+deprecated("Use diskuto.commentstore instead.")
+module diskuto.backend;
+
+public import diskuto.commentstore;
---
+++ source/diskuto/backends/mongodb.d
@@ -0,0 +1,4 @@
+deprecated("Use diskuto.commentstores.mongodb instead.")
+module diskuto.backends.mongodb;
+
+public import diskuto.commentstores.mongodb;
